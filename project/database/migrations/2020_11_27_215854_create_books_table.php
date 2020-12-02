255a50a5bf77e93d889d881b50a3056625a1889c
Cherry Lyn Parreno <cparreno@axxess.com>
2020-12-02 16:09:17 -0600

    Updated books and authors table. Assigned authors_id as foreign key

--- project/database/migrations/2020_11_27_215854_create_books_table.php
+++ project/database/migrations/2020_11_27_215854_create_books_table.php
@@ -15,12 +15,15 @@ class CreateBooksTable extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->unsignedbigInteger('authors_id');
             $table->bigInteger('isbn');
             $table->string('title');
-           // $table->bigInteger('authorid');
-            $table->string('author');
             $table->integer('pages');
             $table->timestamps();
+        });
+
+        Schema::table('books', function($table) {
+            $table->foreign('authors_id')->references('id')->on('authors')->onDelete('cascade');
         });
     }
 
