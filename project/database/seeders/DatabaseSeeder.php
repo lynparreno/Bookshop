0365ebdcd5a39735a74fde23f2f8b8845a712780
Cherry Lyn Parreno <cparreno@axxess.com>
2020-12-02 16:11:14 -0600

    Added seeder for authors table

--- project/database/seeders/DatabaseSeeder.php
+++ project/database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         \App\Models\Book::factory(200)->create();
+      // \App\Models\Author::factory(125)->create();
     }
 }
