cbedb11652e21e99007ca2d311b3e217334c81eb
Cherry Lyn Parreno <cparreno@axxess.com>
2020-12-08 16:23:18 -0600

    Added age and country statistics. Added list of books written per author

--- project/resources/views/authors/authors.blade.php
+++ project/resources/views/authors/authors.blade.php
@@ -8,36 +8,66 @@
             <h1 class="pt-4">List of Authors</h1>
         </div>
     </div>
+    <div class="row">
+        <div class="col-3">
+            <div class="bg-success text-light">
+                <div class="inner">
+                    <h3></h3>
+                    <p>Average Books Written Per Author</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="bg-warning">
+                <div class="inner">
+                    <h3>{{ round($authors->avg('age')) }}</h3>
+                    <p>Average Age</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="bg-primary text-light">
+                <div class="inner">
+                    <h3>{{ $numcountries->count() }}</h3>
+                    <p>No. of Different Countries of Origin</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="row pb-4">
-        <div class="col-6 d-flex justify-content-center">
+        <div class="col-12 d-flex justify-content-end">
             <a class="btn btn-primary" href="#" role="button">Add New Author</a>
         </div>
     </div>
 
     <div class="row">
-        <div class="col-4">
+        <div class="col-12">
             <table class="table table-striped">
                 <thead>
                     <tr>
                         <th>Author's Name</th>
+                        <th>Age</th>
+                        <th>Country of Origin</th>
                     </tr>
                 </thead>
                 <tbody>
                 @foreach($authors as $author)
                 <tr>
                     <td><a href="/authors/{{ $author->id }}">{{ $author->lastname }}, {{ $author->initials }}</td>
+                    <td>{{ $author->age }}</td>
+                    <td>{{ $author->country }}</td>
                 </tr>
                 @endforeach
                 </tbody>
             </table>
-         
-        <div class="row">
-            <div class="col-12 text-left">
-                {{ $authors->links() }}
+
+            <div class="row">
+                <div class="col-12 text-left">
+                    {{ $authors->links() }}
+                </div>
             </div>
-         </div>
-      </div>
-   </div>
-      
+        </div>
+    </div>
 
 @endsection
---
+++ project/resources/views/authors/show.blade.php
@@ -0,0 +1,48 @@
+@extends('layout')
+
+@section('title', 'Author Details')
+
+@section('content')
+<div class="row">
+    <div class="col-12">
+        <form action="" method="POST" class="pb-5">
+            <div class="form-group">
+                <label for="lastname">Author's Last Name:</label>
+                <input type="text" name="lastname" value="{{ $authors->lastname }}" class="form-control">
+            </div>
+            <div class="text-danger">{{ $errors->first('lastname') }}</div>
+
+            <div class="form-group">
+                <label for="initials">Author's Initials:</label>
+                <input type="text" name="initials" value="{{ $authors->initials }}" class="form-control">
+            </div>
+            <div class="text-danger">{{ $errors->first('initials') }}</div>
+
+            <div class="form-group">
+                <label for="age">Age:</label>
+                <input type="text" name="age" value="{{ $authors->age }}" class="form-control">
+            </div>
+            <div class="text-danger">{{ $errors->first('age') }}</div>
+
+            <div class="form-group">
+                <label for="country">Country of Origin:</label>
+                <input type="text" name="country" value="{{ $authors->country }}" class="form-control">
+            </div>
+            <div class="text-danger">{{ $errors->first('country') }}</div>
+
+            <div class="form-group">
+                <label for="listbooks">List of Books Published</label>
+                <ul>
+                    @foreach($books as $book)
+                        <li>{{ $book->title }}</li>
+                    @endforeach
+                </ul>    
+            </div>
+
+            @csrf
+            @method('PATCH')
+            <button class="btn btn-primary" type="submit">Update</button>
+        </form>
+    </div> 
+</div>
+@endsection
