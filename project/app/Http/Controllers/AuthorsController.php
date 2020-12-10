0602203bffcdf9ea6afb65239cb98df5d5578a84
Cherry Lyn Parreno <cparreno@axxess.com>
2020-12-09 18:03:20 -0600

    Added updateAuthor function

--- project/app/Http/Controllers/AuthorsController.php
+++ project/app/Http/Controllers/AuthorsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\Author;
 use App\Models\Book;
 
@@ -12,14 +13,20 @@ class AuthorsController extends Controller
     {
         $books = Book::all();
         $authors = Author::orderBy('lastname', 'ASC')->simplePaginate(50);
-        $averagebooks = Book::distinct('title');
+        $ave_age = Author::all();
         $numcountries = Author::distinct('country');
+        $averagebooks = DB::table('books')
+                        ->select(DB::raw('count(title) as bookcount'))
+                        ->whereNotNull('id')
+                        ->groupBy('authors_id')
+                        ->get(); 
         
         return view('authors.authors', [
             'authors' => $authors,
             'books' => $books,
             'averagebooks' => $averagebooks,
             'numcountries' => $numcountries,
+            'ave_age' => $ave_age,
         ]);
 
     }
@@ -33,5 +40,18 @@ class AuthorsController extends Controller
             'authors' => $authors,
             'books' => $books,
         ]);
+    }
+
+    public function updateAuthor($id)
+    {
+        $authors = Author::find($id);
+
+        $authors->initials = request('initials');
+        $authors->lastname = request('lastname');
+        $authors->age = request('age');
+        $authors->country = request('country');
+
+        $authors->save();
+        return redirect('/authors')->with('success', 'Item has been successfully updated');
     }
 }
