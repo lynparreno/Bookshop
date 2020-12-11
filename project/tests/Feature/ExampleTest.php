000d42f91ca9f25336631bfa198740e31455f54c
Cherry Lyn Parreno <cparreno@axxess.com>
2020-12-11 14:19:26 -0600

    First Test

--- project/tests/Feature/ExampleTest.php
+++ project/tests/Feature/ExampleTest.php
@@ -4,9 +4,13 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\Book;
+use App\Models\Author;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      *
@@ -17,5 +21,22 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function testBookTitleCanBeSeenOnTitlePage()
+    {
+        //arrange
+        $author = Author::factory()->create();
+        Book::factory()->create([
+            'title'=> 'The Cat Who Said Cheese',
+            'author_id' => $author->id,
+        ]);
+
+        //action
+        $response = $this->get('/books');
+
+        //assert
+        $response->assertStatus(200);
+        $response->assertSee('The Cat Who Said Cheese');
     }
 }
